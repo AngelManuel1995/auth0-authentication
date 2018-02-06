c1455046f866324302ac2ba77f6e49911bb6e29d
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-06 15:05:48 -0500

    Protected component ready to use

---
+++ src/app/components/protected/protected.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector:'protected-component',
+    templateUrl:'./protected.component.html'
+})
+
+export class ProtectedComponent implements OnInit{
+    
+    constructor(){
+
+    }
+
+    ngOnInit(){
+        
+    }
+}
