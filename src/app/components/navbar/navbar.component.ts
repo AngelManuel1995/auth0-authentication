84f9c5624baf2d2e77b08d223a8945ffae1c716e
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-06 15:04:16 -0500

    Navbar component ready to use

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector:'navbar-component',
+    templateUrl:'./navbar.component.html'
+})
+
+export class NavbarComponent implements OnInit{
+
+    constructor(){
+
+    }
+
+    ngOnInit(){
+        
+    }
+}
