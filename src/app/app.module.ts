75aa430e6c51e279b3e1fb8f7b0de4563eb1b9ef
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-07 20:53:03 -0500

    AuthGuardSerive added to this file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }      from '@angular/platform-browser';
+import { NgModule }           from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { PricesComponent } from './components/prices/prices.component';
+import { AppComponent }       from './app.component';
+import { HomeComponent }      from './components/home/home.component';
+import { NavbarComponent }    from './components/navbar/navbar.component';
+import { PricesComponent }    from './components/prices/prices.component';
 import { ProtectedComponent } from './components/protected/protected.component';
-import { APP_ROUTING } from './app.routes';
+import { APP_ROUTING }        from './app.routes';
 
 
 //Services
-import { AuthService } from './services/auth.service';
+import { AuthService }        from './services/auth.service';
+import { AuthGuardService }   from './services/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,9 @@ import { AuthService } from './services/auth.service';
   ],
   providers: [
     //Services
-    AuthService
+    AuthService,
+    AuthGuardService
+
   ],
   bootstrap: [AppComponent]
 })
