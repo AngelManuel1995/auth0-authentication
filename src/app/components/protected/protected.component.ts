0361907bbbcc9f614098536e2c158dda9d975600
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-02-08 19:19:07 -0500

    Using the user's data

--- src/app/components/protected/protected.component.ts
+++ src/app/components/protected/protected.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector:'protected-component',
@@ -7,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 
 export class ProtectedComponent implements OnInit{
     
-    constructor(){
+    constructor( private auth:AuthService){
 
     }
 
+    profile: any;
+
     ngOnInit(){
         
+        if (this.auth.userProfile) {
+            this.profile = this.auth.userProfile;
+        } else {
+            this.auth.getProfile((err, profile) => {
+            this.profile = profile;
+           });
+        }
+
     }
 }
